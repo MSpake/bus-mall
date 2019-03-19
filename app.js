e0b7c28063ec6f321d3b75cd9cc272e0dd897c8e
mspake <morgana.spake@gmail.com>
2019-03-18 21:47:01 -0700

    click tracking and image rendering working correctly

--- app.js
+++ app.js
@@ -6,17 +6,22 @@
 
 var total_clicks = 0;
 var all_product_images = [];
-var left_image_on_page;
-var center_image_on_page;
-var right_image_on_page;
+var products_on_page = [];
+var how_many_products = document.getElementsByClassName('product');
+
 //-----------------------------
 // Constructors and Associated Methods
 //-----------------------------
 
+//Product_image constructor
+//takes the image path and the name of the product
+//sets all starting clicks and times shown on page to 0
+//assigns an element name (product name, all lowercase, with whitespce removed) that can be used to identify it when it's on the page
+//adds each Product_image instance to the all_product_images array
 var Product_image = function(url, product_name) {
   this.url = url;
   this.product_name = product_name;
-  this.id = product_name.toLowerCase().replace(/\s/g, '');
+  this.element_name = product_name.toLowerCase().replace(/\s|-/g, '');
   this.clicks = 0;
   this.times_shown_on_page = 0;
 
@@ -28,57 +33,73 @@ var Product_image = function(url, product_name) {
 // Stand Alone Functions
 //-----------------------------
 
+//function to generate a random number
+//range set by length of all_product_images array
 function select_random_image() {
   var random_image_index = Math.floor(Math.random() * all_product_images.length);
-  all_product_images[random_image_index].times_shown_on_page++;
   return random_image_index;
 }
+// all_product_images[random_image_index].times_shown_on_page++;
 
-function set_all_images() {
+function new_image_set() {
   var new_images_to_render_to_page = [];
-  for (var i = 0; i < 3; i++) {
+
+  //selects random images to render using the select_random_image function
+  //number of images to select is based on how many elements have the 'product' class
+  for (var i = 0; i < how_many_products.length; i++) {
     var new_image = select_random_image();
+    //prevent duplicate images by checking if the image's index has already been generated and generating a new index until they are all unique
     while (new_images_to_render_to_page.includes(new_image)) {
       new_image = select_random_image();
     }
+    //push to the new_images_to_render array
     new_images_to_render_to_page.push(new_image);
+
+  }
+  return new_images_to_render_to_page;
+
+}
+
+//renders three new images to the page
+function render_new_images() {
+  var new_images_to_render_to_page = new_image_set();
+
+  //for each product element on the page, update the 'src' and 'name' attributes
+  //use the values stored in the 'url' and 'element_name' properties of the replacement image object
+  for (var j = 0; j < how_many_products.length; j++) {
+    //the newly generated index
+    var new_image = new_images_to_render_to_page[j];
+    products_on_page[j] = all_product_images[new_image];
+
+    //target the specific image element
+    var image_element = how_many_products[j].childNodes[1];
+    //update the 'src' and 'name' attributes with the new information
+    image_element.src = products_on_page[j].url;
+    image_element.name = products_on_page[j].element_name;
   }
 
-  console.log(new_images_to_render_to_page);
-
-  left_image_on_page = all_product_images[new_images_to_render_to_page[0]];
-  center_image_on_page = all_product_images[new_images_to_render_to_page[1]];
-  right_image_on_page = all_product_images[new_images_to_render_to_page[2]];
-
-
-  var left_image = document.getElementById('left_product_image');
-  var center_image = document.getElementById('center_product_image');
-  var right_image = document.getElementById('right_product_image');
-
-  left_image.src = left_image_on_page.url;
-  center_image.src = center_image_on_page.url;
-  right_image.src = right_image_on_page.url;
+  //update the products_on_page array with the newly rendered products' indexes
+  products_on_page = new_images_to_render_to_page;
 }
 
 function image_was_clicked(event) {
-  total_clicks++;
 
-  if (event.target.id === 'left_product_image') {
-    left_image_on_page.clicks++;
-    // console.log(left_image_on_page);
-    // console.log(left_image_on_page.clicks);
+  //limit to 25 clicks
+  if (total_clicks < 5) {
+    var clicked;
+    for (var k = 0; k < how_many_products.length; k++) {
+      var possible_targeted_product = products_on_page[k];
+      if (all_product_images[possible_targeted_product].element_name === event.target.name) {
+        clicked = all_product_images[possible_targeted_product];
+        clicked.clicks++;
+        total_clicks++;
+        render_new_images();
+        console.log(clicked);
+      }
+    }
+  } else {
+    product_choices.removeEventListener('click', image_was_clicked);
   }
-  if (event.target.id === 'center_product_image') {
-    center_image_on_page.clicks++;
-    // console.log(center_image_on_page);
-    // console.log(center_image_on_page.clicks);
-  }
-  if (event.target.id === 'right_product_image') {
-    right_image_on_page.clicks++;
-    // console.log(right_image_on_page);
-    // console.log(right_image_on_page.clicks);
-  }
-  set_all_images();
 }
 
 //-----------------------------
@@ -107,16 +128,15 @@ new Product_image('./img/usb.gif', 'Tentacle USB Drive');
 new Product_image('./img/water-can.jpg', 'Watering Can');
 new Product_image('./img/wine-glass.jpg', 'Wine Glass');
 
-
+console.log(all_product_images);
 //starts page with three random images
-set_all_images();
+render_new_images();
 
 
-
+// console.log(how_many_products);
 
 
 //clicking
 var product_choices = document.getElementById('product_choices');
-// console.log(product_choices);
 
 product_choices.addEventListener('click', image_was_clicked);
